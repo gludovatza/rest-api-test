a83eeb5ffed791d4193feb4134d9a5ac3be92ddc
gludovatza <attila@gludovatz.hu>
2023-06-18 18:40:51 +0200

    Article api routes and controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('articles', [ArticleController::class,'index']);
+Route::get('articles/{article}', [ArticleController::class,'show']);
+Route::post('articles', [ArticleController::class,'store']);
+Route::put('articles/{article}', [ArticleController::class,'update']);
+Route::delete('articles/{article}', [ArticleController::class,'delete']);
